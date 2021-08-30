e655604743f24e4a86cf9f8a80e541c0a8ca24e2
DESKTOP-64VHHIQ\rha45 <rha4578@gmail.com>
2021-08-30 23:50:19 +0900

    naver map interface setting

--- app/src/main/java/com/example/app/MapViewActivity.java
+++ app/src/main/java/com/example/app/MapViewActivity.java
@@ -1,24 +1,38 @@
 package com.example.app;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.widget.LocationButtonView;
+import com.naver.maps.map.widget.ZoomControlView;
 
 public class MapViewActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final int Location_PERMISSION_REQUEST_CODE = 1000;
+    private NaverMap naverMap;
     private MapView mapView;
+    private FusedLocationSource locationSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_fragment_layout);
-        // ...
 
+        locationSource = new FusedLocationSource(this, Location_PERMISSION_REQUEST_CODE);
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
+
+
     }
 
     @Override
@@ -64,8 +78,28 @@ public class MapViewActivity extends AppCompatActivity implements OnMapReadyCall
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if(!locationSource.isActivated()) {
+                naverMap.setLocationTrackingMode(LocationTrackingMode.NoFollow);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
     public void onMapReady(NaverMap naverMap) {
-        //...
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setZoomControlEnabled(false);
+        uiSettings.setLocationButtonEnabled(true);
+
+        LocationButtonView locationButton = findViewById(R.id.mapLocation);
+        locationButton.setMap(naverMap);
+        naverMap.setLocationSource(locationSource);
+        ZoomControlView zoomControlView = findViewById(R.id.mapZoom);
+        zoomControlView.setMap(naverMap);
     }
 }
 
